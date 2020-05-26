b41f5c6581941516b1ee68b9dd96c8067b684564
AlessandroFilino <65735750+AlessandroFilino@users.noreply.github.com>
2020-05-26 09:19:10 +0200

    Add files via upload

---
+++ sistemiOperativi/sistemiOperativi/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sistemiOperativi
+//
+//  Created by Alessandro Filino on 26/05/2020.
+//  Copyright © 2020 Alessandro Filino. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
